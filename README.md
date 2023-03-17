0b395c53d7a23f1ea74b827312a5e2e460be7be8
MoAzFecO <jules.feron@gmail.com>
2023-03-17 14:36:02 +0100

    commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Projet_IA : Identification d’ensembles  de gènes par  programmation logique
+# Projet IA : Identification d’ensembles  de gènes par  programmation logique
 
 Ce repository contient les codes pour identifier les gènes à partir de matrices de cellules et de gènes en utilisant l'ASP (Answer Set Programming).
 ## Contenu du repository
@@ -8,9 +8,16 @@ Le repository est organisé en trois dossiers :
 - `matrices` : contient les matrices de cellules et de gènes
 - `résultats` : contient des résultats obtenus
 ## Pour exécuter le code
-Pour exécuter les algorithmes ASP, on utilise la commande : clingo algorithmes/nom_algo 
+Pour exécuter le code python :
+```
+python3 algorithmes/atome.py
+```
+Pour exécuter les algorithmes ASP : 
+```
+clingo algorithmes/nom_algo
+```
 
-On peut rajouter des arguments dans la ligne de commande :
-- `-t n` : pour utiliser le multithreading avec n threads (n<64)
-- `--time-limit=n` : pour limiter le temps d'exécution de l'algorithme à n secondes
+On peut rajouter des arguments dans la ligne de commande ASP :
 - `0 --project --opt-mod=enum,n` : pour faire une projection avec pour valeur d'optimisation n
+- `--time-limit=n` : pour limiter le temps d'exécution de l'algorithme à n secondes
+- `-t n` : pour utiliser le multithreading avec n threads (n<64)
--- algorithmes/atome.py
+++ algorithmes/atome.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import sys
+#sys.path.append("..")
 
 def create_atom(chemin, sortie):
     données_data =pd.read_csv(chemin)  # chemin csv à lire
@@ -18,7 +20,8 @@ def create_atom(chemin, sortie):
                     str(données[i,2]).lower()+", " +  # stage de la cellule
                     données_data.columns[j].lower() +", " +  # nom du gène
                     str(données[i,j]) +").\n")  # expression du gène (0 ou 1)
-                
+
+
 def create_atom2(chemin, sortie): # pour l'algo ASP de Anran et Li
     données_data =pd.read_csv(chemin)  # chemin csv à lire
     données = données_data.values
@@ -61,7 +64,7 @@ def lire_txt(matrice, chemin, sortie):
         if 'selec_gene' in mot :
             genes.append(mot[11:-1].upper())
    
-    data_pd =pd.read_csv(f'données/{matrice}.csv')
+    data_pd =pd.read_csv(f'matrices/{matrice}.csv')
     nom_cell = data_pd.values[:,0] # nom des cellules
     num_cell = []  # numéro des cellules
     for k in range(len(affinites)) : 
@@ -82,6 +85,7 @@ def lire_txt(matrice, chemin, sortie):
             df.at[affinite[1:-1], gene] = column.iloc[num_cell[k]]
     print(df)
     df.to_csv(f"{sortie}.csv")
+
 
 def lire_projection(chemin, nom_sortie):
     f = open(chemin, "r")
@@ -105,6 +109,7 @@ def lire_projection(chemin, nom_sortie):
     print(df)
     df.to_csv(nom_sortie + ".csv", index=False)
 
+
 def lire_plusieurs_classes(matrice, chemin, sortie):
     f = open(chemin,"r")
     texte = str(f.read())   
@@ -127,7 +132,7 @@ def lire_plusieurs_classes(matrice, chemin, sortie):
         if 'selec_gene' in mot :
             genes.append(mot[11:-1].upper())
    
-    data_pd =pd.read_csv(f'données/{matrice}.csv')
+    data_pd =pd.read_csv(f'matrices/{matrice}.csv')
     nom_cell = data_pd.values[:,0] # nom des cellules
     for numero in num_match: 
         nom_match.append(nom_cell[numero])
@@ -140,7 +145,69 @@ def lire_plusieurs_classes(matrice, chemin, sortie):
     print(df)
     df.to_csv(f"{sortie}.csv")
 
+
+def lire_plusieurs_classes_avec_stages(matrice, chemin, sortie):
+    f = open(chemin,"r")
+    texte = str(f.read())   
+    x = texte.split()
+
+    num_match = []
+    nom_match = []
+    genes = []
+
+    # indice du dernier answer dans la sortie
+    indice = 0
+    for k, mot in enumerate(x) :
+        if 'Answer' in mot :
+            indice = k
+    x = x[indice:] # on supprime le texte qui précède answer
+
+    for mot in x:
+        if 'match' in mot :
+            num_match.append(int(mot[6:-1]))
+        if 'selec_gene' in mot :
+            genes.append(mot[11:-1].upper())
+   
+    data_pd =pd.read_csv(f'matrices/{matrice}.csv')
+    nom_cell = data_pd.values[:,0] # nom des cellules
+    for numero in num_match: 
+        nom_match.append(nom_cell[numero])
+
+    df = pd.DataFrame(columns=genes)
+    for gene in genes : 
+        column = data_pd[gene]
+        for k, match in enumerate(nom_match) :
+            df.at[match, gene] = column.iloc[num_match[k]]
+
+    data_pd = data_pd.set_index(nom_cell)
+    matrice_cell = [{} for k in range(len(nom_match))]
+    for cell in range(len(nom_match)):
+        vecteur = df.values[cell,:]
+        for ligne in nom_cell:
+            m_vecteur = True
+            for k, gene in enumerate(genes):
+                if vecteur[k] != data_pd.at[ligne, gene]:
+                    m_vecteur = False
+                    break
+            if m_vecteur:
+                stage = data_pd.at[ligne,'clusterUmap']
+                dico = matrice_cell[cell]
+                if not stage in dico:
+                    matrice_cell[cell][stage] = [ligne]
+                else :
+                    matrice_cell[cell][stage].append(ligne)
+    for stage in matrice_cell[0].keys():
+        df[stage]=''
+    for k, match in enumerate(nom_match):
+        for stage in matrice_cell[0].keys():
+            df.at[match, stage] = ', '.join(matrice_cell[k][stage])
+                
+    print(df)
+    df.to_csv(f"{sortie}.csv")
+
+
 def create_all_atom():
+    """Fonction qui permet de créer toutes les instances ASP des différentes matrices pour les algorithmes ASP"""
     create_atom("matrices/B.csv", "données/B.lp")
     create_atom("matrices/C.csv", "données/C.lp")
     create_atom("matrices/D.csv", "données/D.lp")
@@ -154,10 +221,13 @@ def create_all_atom():
     create_atom2("matrices/B.csv", "données/anran_li/C.lp")
     create_atom2("matrices/B.csv", "données/anran_li/D.lp")
 
+
 if __name__ == "__main__" :
+    """Des exemples d'exécution des fonctions précédentes sont commentées ci-dessous"""
     #create_all_atom()
-    #lire_txt("B", "test0.txt", "sortie3")
-    #lire_projection("projection.txt", 'projection')
-    #lire_plusieurs_classes("B_3_classes", "B_3_classes.txt", "B_3_classes_k=4")
-    #lire_projection("projection_3_classes.txt", 'projection_3_classes')
+    #lire_txt("B", "résultats/sortie_ASP/B_K3.txt", "résultats/vecteurs_B_K3")
+    #lire_projection("résultats/sortie_ASP/projection_B_K3.txt", 'résultats/projection_B_K3')    
+    #lire_projection("résultats/sortie_ASP/projection_B_3_classes_K4.txt", 'résultats/projection_B_3_classes_K4')
+    #lire_plusieurs_classes("B_3_classes", "résultats/sortie_ASP/k_classes_B_3_K4.txt", "résultats/vecteurs_B_3_classes_K4")
+    #lire_plusieurs_classes_avec_stages("D_3_classes", "résultats/sortie_ASP/k_classes_D_3_K10.txt", "résultats/stages_vecteurs_D_3_K10")
     pass
